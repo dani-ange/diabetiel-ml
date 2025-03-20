4ed9e1af7b960212f8c82f634c0ae2425de5329d
dani-ange <danielledjofang2003@gmail.com>
2025-03-20 13:32:21 +0100

    read

--- app.py
+++ app.py
@@ -11,12 +11,6 @@ st.write("Enter patient data to predict diabetes")
 
 # Input fields
 st.header("welcome to the appa")
-st.button("welcom")
-st.button("aged")
-st.button("ageddd")
-st.button("agessssss")
-
-
 
 pregnancies = st.number_input("Pregnancies", min_value=0, max_value=20)
 glucose = st.number_input("Glucose Level", min_value=0, max_value=200)
